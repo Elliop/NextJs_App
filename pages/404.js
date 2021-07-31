aed8c5df972153f672f238c8d2621b1292271021
Elliop <abdelmouhayminegx@gmail.com>
2021-07-31 13:55:19 +0100

    Redirecting Users

--- pages/404.js
+++ pages/404.js
@@ -1,15 +1,28 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      // router.go(-1)
+      // router.go(1)
+      router.push("/");
+    }, 3000);
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Ooops...</h1>
       <h2>That page cannot be found :(</h2>
       <p>
-        Go back to the{" "}
+        Going back to the
         <Link href="/">
-          <a>Homepage</a>
-        </Link>
+          <a> Homepage</a>
+        </Link>{" "}
+        is 3 seconds...
       </p>
     </div>
   );
