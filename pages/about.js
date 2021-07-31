a6eb757d130ec79e6c566e3f88a399e1c053a289
Elliop <abdelmouhayminegx@gmail.com>
2021-07-31 14:06:13 +0100

    Add Images & Metadata

--- pages/about.js
+++ pages/about.js
@@ -1,22 +1,34 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industrys standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries
-      </p>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industrys standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
+          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
+          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
+          sequi!
+        </p>
+      </div>
+    </>
   );
 };
 
